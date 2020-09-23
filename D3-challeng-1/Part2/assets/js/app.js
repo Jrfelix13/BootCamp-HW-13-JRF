126fda2deb7cc8bc9c98f26a446bf0afef9fecab
Jrfelix13 <juan.r.felix13@gmail.com>
2020-09-23 17:41:49 -0500

    Update app.js

--- D3-challeng-1/Part2/assets/js/app.js
+++ D3-challeng-1/Part2/assets/js/app.js
@@ -1,13 +1,5 @@
 // Create the size of the chart
-var svgWidth = 960;
-var svgHeight = 500;
 
-var margin = {
-    top: 20,
-    right: 40,
-    bottom: 100,
-    left: 100
-};
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
